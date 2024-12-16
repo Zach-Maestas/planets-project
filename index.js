aa6e86f057809d61c2aa67a97e277759c0ddca2a
Zach Maestas <135179050+Zach-Maestas@users.noreply.github.com>
2024-12-16 17:08:36 -0500

    Reordered Functions

--- index.js
+++ index.js
@@ -10,49 +10,6 @@ const parser = parse({
     columns: true,
 });
 
-function isHabitablePlanet(planet){
-    return planet.koi_disposition == 'CONFIRMED'
-    && planet.koi_insol > 0.36 && planet.koi_insol < 1.11
-    && planet.koi_prad < 1.6;
-}
-
-function extractColumns(){
-    return Object.keys(habitablePlanets[0]).reduce((acc, key) => {
-        acc[key] = key;  // Map each key to itself
-        return acc;
-    }, {});
-}
-
-function writeToCSV(csvString) {
-    fs.writeFile('habitablePlanets.csv', csvString, (err) => {
-        if (err) {
-            console.error('Error writing CSV file:', err);
-            return;
-        }
-        console.log('Successfully saved habitable planets to CSV!');
-    });
-}
-
-function habitablePlanetsToCSV() {
-    if (habitablePlanets.length === 0) {
-        console.log("No habitable planets to save.");
-        return;
-    }
-
-    const columns = extractColumns(); 
-
-    stringify(habitablePlanets, {
-        header: true,
-        columns: columns
-    }, (err, output) => {
-        if (err) {
-            console.error('Error stringifying data:', err);
-            return;
-        }
-        writeToCSV(output);  // Pass the CSV string output to writeToCSV
-    });
-}
-
 readableStream
     .pipe(parser)
     .on('data', (data) => {
@@ -76,3 +33,46 @@ readableStream.on('error', (err) => {
 parser.on('error', (err) => {
     console.error('Error while parsing CSV:', err);
 });
+
+function isHabitablePlanet(planet){
+    return planet.koi_disposition == 'CONFIRMED'
+    && planet.koi_insol > 0.36 && planet.koi_insol < 1.11
+    && planet.koi_prad < 1.6;
+}
+
+function habitablePlanetsToCSV() {
+    if (habitablePlanets.length === 0) {
+        console.log("No habitable planets to save.");
+        return;
+    }
+
+    const columns = extractColumns(); 
+
+    stringify(habitablePlanets, {
+        header: true,
+        columns: columns
+    }, (err, output) => {
+        if (err) {
+            console.error('Error stringifying data:', err);
+            return;
+        }
+        writeToCSV(output);  // Pass the CSV string output to writeToCSV
+    });
+}
+
+function extractColumns(){
+    return Object.keys(habitablePlanets[0]).reduce((acc, key) => {
+        acc[key] = key;  // Map each key to itself
+        return acc;
+    }, {});
+}
+
+function writeToCSV(csvString) {
+    fs.writeFile('habitablePlanets.csv', csvString, (err) => {
+        if (err) {
+            console.error('Error writing CSV file:', err);
+            return;
+        }
+        console.log('Successfully saved habitable planets to CSV!');
+    });
+}
